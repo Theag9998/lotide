4fcbbd0fe725c1f102afb7db24bf324f3d2db088
Thea Ganden <wtp9998@hotmail.com>
2020-04-01 22:32:30 +0000

    Key concepts of objects

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+	if (actual === expected) {
+	console.log( `🙂Assertion Passed: ${actual} === ${expected}`)
+	} else {
+	console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`)
+	}
+};
+
+const countOnly = function (allItems, itemsToCount) {
+let results = {}
+
+for (const item of allItems) {
+	console.log(item)
+	if (itemsToCount[item]) {
+		if (results[item]) {
+			results[item] += 1;
+		} else {
+			results[item] = 1;
+		}
+	}
+}
+return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);

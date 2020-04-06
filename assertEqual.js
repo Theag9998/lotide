89e60e847c818e62a2ccdb94d4b3f8093bc5a807
Thea Ganden <wtp9998@hotmail.com>
2020-04-06 19:19:02 +0000

    Refactoring lotide

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 	if (actual === expected) {
-	console.log( "🙂Assertion Passed: " + actual +  " === " + expected)
+	console.log( `🙂Assertion Passed: ${actual} === ${expected}`)
 	} else {
-	console.log("☹️ Assertion Failed: " + actual + " !== " + expected)
+	console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`)
 	}
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,8 @@
-const assertEqual = function(actual, expected) {
-	if (actual === expected) {
-	console.log( `🙂Assertion Passed: ${actual} === ${expected}`)
-	} else {
-	console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`)
-	}
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (actual) {
 	return actual[0]
 
 }
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
